65924f64eedb00d9d350bb4826dac1b4e5540498
jabattack1 <peter.hahn@live.com>
2015-04-21 19:42:21 -0400

    about and resume

--- peter/app/assets/javascripts/scripts.js
+++ peter/app/assets/javascripts/scripts.js
@@ -1,9 +1,15 @@
 console.log("MOTHA!");
 
+var template = "<h3 class='animated fadeIn'>Peter Hahn</h3>";
+var headshot = "<img src='http://i1301.photobucket.com/albums/ag119/jabattack1/headshot_zpssoavnc6u.jpg' class='animated fadeIn' id='photo'>";
+var resume = "<iframe src=\"https://docs.google.com/file/d/0B_QUho3Z8WoPck0xRjA5akRvYjA/preview\" width=\"740\" height=\"580\" class='animated fadeIn' id='resume'></iframe>";
+
 function about_onClick(e) {
 	console.log('about');
+
 	$('#content').empty();
-	$('#content').append("<p>Peter Hahn</p>");
+	$('#content').append(template);
+	$('#content').append(headshot);
 	// e.preventDefault();
 	// return false;
   }
@@ -11,6 +17,7 @@ function about_onClick(e) {
 function resume_onClick() {
 	console.log('resume');
 	$('#content').empty();
+	$('#content').append(resume);
    // alert('a_onClick');
   }
 
